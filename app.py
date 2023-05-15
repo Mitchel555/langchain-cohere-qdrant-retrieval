c556a31c0b417f109cab324bacc65f2d79edba3d
Mitchel555 <131654141+Mitchel555@users.noreply.github.com>
2023-05-15 22:46:57 +0200

    Update app.py - experimental

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, request
 from flask_cors import CORS
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import ElasticVectorSearch, Pinecone, Weaviate, FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.chains.question_answering import load_qa_chain
 import json
 
 # Loading environment variables
@@ -32,16 +37,24 @@ def embed_pdf():
 
     loader = PyPDFLoader(file_url)
     docs = loader.load_and_split()
-    embeddings = CohereEmbeddings(model="multilingual-22-12", cohere_api_key=cohere_api_key)
+
+    embeddings = OpenAIEmbeddings(openai_api_key)  # replacing CohereEmbeddings with OpenAIEmbeddings
+
     qdrant = Qdrant.from_documents(docs, embeddings, url=qdrant_url, collection_name=collection_name, prefer_grpc=True, api_key=qdrant_api_key)
     
-    return {"collection_name":qdrant.collection_name}
+    return {"collection_name": qdrant.collection_name}
 
 # Retrieve information from a collection
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from qdrant_client import QdrantClient
+from langchain.prompts import PromptTemplate
+from langchain.prompts.chat import ChatPromptTemplate, HumanMessagePromptTemplate
+
+from langchain.llms import OpenAI
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
 
 @app.route('/retrieve', methods=['POST'])
 def retrieve_info():
@@ -50,10 +63,31 @@ def retrieve_info():
 
     client = QdrantClient(url=qdrant_url, prefer_grpc=True, api_key=qdrant_api_key)
 
-    embeddings = CohereEmbeddings(model="multilingual-22-12", cohere_api_key=cohere_api_key)
+    embeddings = OpenAIEmbeddings(openai_api_key) 
     qdrant = Qdrant(client=client, collection_name=collection_name, embedding_function=embeddings.embed_query)
-    search_results = qdrant.similarity_search(query, k=2)
-    chain = load_qa_chain(ChatOpenAI(openai_api_key=openai_api_key,temperature=0.2), chain_type="stuff")
-    results = chain({"input_documents": search_results, "question": query}, return_only_outputs=True)
-    
-    return {"results":results["output_text"]}
+    search_results = qdrant.similarity_search(query, k=5)
+
+    # Define the LLM and the prompt
+    chat = ChatOpenAI(temperature=0)
+    human_message_prompt = HumanMessagePromptTemplate(
+        prompt=PromptTemplate(
+            template="Zpracuj odpoved na zaklade uzivatelskeho vstupu: {query} Poskytnuty Text:{search_results} Pokud odpověď v tomto textu nenajdes, napiš že nevíš",
+            input_variables=["query","search_results"],
+        )
+    )
+    chat_prompt_template = ChatPromptTemplate.from_messages([human_message_prompt])
+
+    # Define the chain
+    chain = LLMChain(llm=chat, prompt=chat_prompt_template)
+
+    # Prepare the input variables for the chain
+    input_vars = {"search_results": search_results, "query": query}
+
+    # Print the prompt
+    # prompt = "Zpracuj odpoved na zaklade uzivatelskeho vstupu: {query} Poskytnuty Text:{search_results} Pokud odpověď v tomto textu nenajdes, napiš že nevíš".format(query=query, search_results=search_results)
+    # print(f'Prompt sent to OpenAI: {prompt}')
+
+    # Run the chain
+    results = chain.run(input_vars)
+
+    return {"results": results}
